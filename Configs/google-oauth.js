7439fbddcc44d9a4d503f2825ae41eb58c66c317
shoaib patel <100205704+PatelShoaib7@users.noreply.github.com>
2023-01-23 19:59:20 +0530

    Create google-oauth.js

--- Configs/google-oauth.js
+++ Configs/google-oauth.js
@@ -1,3 +1,4 @@
+
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const passport = require('passport');
 const userMOdel = require('../Models/User');
@@ -5,21 +6,23 @@ require('dotenv').config();
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://oauthg.onrender.com/auth/google/callback",
+    callbackURL: "https://oauthg.onrender.com/google/callback",
     scope: ['profile','email']
   },
   async function(accessToken, refreshToken, profile, cb) {
     // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-       let user = profile._json;
-       //console.log(user)
-       const {sub , name , email , picture} = user;
-       const findUser = await userMOdel.findOne({$or:[{email} ,{id:sub}]})
-       console.log(findUser)
-       if(findUser== null){
-        const saveUser = await userMOdel({name , email, img:picture ,id:sub});
-        saveUser.save();
-       }
-       return cb(null, profile );
+      console.log(profile._json)
+      console.log(accessToken)
+      let user = profile._json;
+      //console.log(user)
+      const {sub , name , email , picture} = user;
+      const findUser = await userMOdel.findOne({$and:[{name} ,{id:sub}]})
+      console.log(findUser)
+      if(findUser == null){
+       const saveUser = await userMOdel({name , email, img:picture ,id:sub});
+       saveUser.save();
+      }
+       return cb(null, profile , accessToken);
     // })
   }
 ));
